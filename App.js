e45ed99b4d85c8177263d3158375ac6aba41146c
JeremyOlea <43157357+JeremyOlea@users.noreply.github.com>
2020-05-24 22:01:48 -0600

    converted to html program now

---
+++ App.js
@@ -0,0 +1,47 @@
+
+// Globals
+let sudoku = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
+  ];
+
+const colors = [
+"baby-blue", "teal", "orange", "purple", "yellow", "green", "turquoise", "pink", "blue"
+]
+
+function start() {
+    console.log("here in app.js");
+    let body = document.getElementsByTagName("BODY")[0];
+    let grid = document.createElement('div');
+    grid.classList.add('grid');
+    for(let i = 0; i < 3*3; i++) { //creates 10 nested 'div' elements for the grid
+        let content = document.createElement('div');
+        content.classList.add('grid-block');
+        
+        for(let j = 0; j < 3*3; j++){
+        let cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.classList.add(colors[i])
+        
+        let textField = document.createElement('p');
+        textField.classList.add('cell-text')
+        textField.setAttribute('id', i + '-' + j);
+        // textField.onclick = cellClicked;
+
+        cell.append(textField);
+        content.append(cell)
+        }
+        grid.appendChild(content);
+    }
+    body.appendChild(grid);
+}
+
+start();
